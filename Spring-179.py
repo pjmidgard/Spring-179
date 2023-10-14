02ce9ddc5622f1ebbe9addcdac5e237edc9d7399
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-10-14 16:33:49 +0100

    Add files via upload

--- Spring-179.py
+++ Spring-179.py
@@ -1,12 +1,12 @@
-#Author Jurijus Pacalovas 
+#Author Jurijus Pacalovas
 import paq
 import os
 
-# Function to compress data
+# Function to compress data using "paq"
 def compress_data(input_data):
     return paq.compress(input_data)
 
-# Function to extract data
+# Function to extract data using "paq"
 def extract_data(compressed_data):
     return paq.decompress(compressed_data)
 
@@ -17,17 +17,28 @@ X3 = 0
 X1_limit = 2**24
 compressed_data = None
 
-# Check if a saved compressed data file exists and load it
-if os.path.exists("compressed_data.gz"):
-    with open("compressed_data.gz", "rb") as saved_data_file:
-        compressed_data = saved_data_file.read()
+# Define the hidden folder name
+hidden_folder = ".bin"
+
+# Define the hidden file name in the hidden folder
+hidden_compressed_file = f"{hidden_folder}/.my_hidden_data.paq"
+
+# Check if the hidden folder exists, and create it if not
+if not os.path.exists(hidden_folder):
+    os.mkdir(hidden_folder)
 
 # Automatically extract data when the program starts
 if compressed_data is not None:
     extracted_data = extract_data(compressed_data)
-    with open("extracted_data.bin", "wb") as extracted_file:
+    extracted_file_name = "extracted_data.bin"
+    # Check if the file exists, and choose a different name if it does
+    file_counter = 1
+    while os.path.exists(extracted_file_name):
+        extracted_file_name = f"extracted_data_{file_counter}.bin"
+        file_counter += 1
+    with open(extracted_file_name, "wb") as extracted_file:
         extracted_file.write(extracted_data)
-    print("Data has been automatically extracted.")
+    print(f"Data has been automatically extracted using 'paq' and saved to {extracted_file_name}")
 
 while True:
     # Ask the user for their choice
@@ -51,15 +62,20 @@ while True:
     elif choice == "2":
         if compressed_data is not None:
             extracted_data = extract_data(compressed_data)
-            extracted_file_name = input("Enter the name for extracted file: ")
+            extracted_file_name = input("Enter the name for manual extraction: ")
+            # Check if the file exists, and choose a different name if it does
+            file_counter = 1
+            while os.path.exists(extracted_file_name):
+                extracted_file_name = f"{extracted_file_name}_{file_counter}"
+                file_counter += 1
             with open(extracted_file_name, "wb") as extracted_file:
                 extracted_file.write(extracted_data)
-            print(f"Data has been manually extracted and saved to {extracted_file_name}")
+            print(f"Data has been manually extracted using 'paq' and saved to {extracted_file_name}")
 
     else:
-        # Save the compressed data to a file before exiting
+        # Save the compressed data to a hidden file in the hidden folder before exiting
         if compressed_data:
-            with open("compressed_data.paq", "wb") as saved_data_file:
+            with open(hidden_compressed_file, "wb") as saved_data_file:
                 saved_data_file.write(compressed_data)
         print("Exiting.")
         break
